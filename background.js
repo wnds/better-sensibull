581b2ddf13b22fcbe6d74a891933557c11e9fc12
wnds <singh.kr.vivek@gmail.com>
2023-05-10 21:10:55 +0530

    This commit adds changes to execute browserify during release zip creation. The commit also adds changes to create zip with version included.

--- background.js
+++ background.js
@@ -1,36 +1,86 @@
-const cheerio = require('cheerio');
+const fs = require('fs');
+const path = require('path');
+const archiver = require('archiver');
+const { exec } = require('child_process');
 
-async function fetchUpcomingHoliday() {
-  try {
-    const response = await fetch('https://zerodha.com/marketintel/holiday-calendar/', {
-      headers: {
-        'Cache-Control': 'no-cache',
-      },
-    });
-    const html = await response.text();
-    const $ = cheerio.load(html);
+const manifest = require('./manifest.json'); // Import the manifest file
+const gitignorePath = path.join(__dirname, '.gitignore');
+const outputPath = path.join(__dirname, `better-sensibull-v${manifest.version}.zip`); // Include version number in the filename
 
-    const holidayText = $('#holidays h4.text-center').text()
-
-    return holidayText.trim();
-  } catch (error) {
-    console.error('Error fetching holiday data:', error);
+const isIgnored = (filepath) => {
+  if (filepath === 'backgroundBundle.js') {
+    return false;
   }
-}
 
-chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
-  if (request.action === 'getUpcomingHoliday') {
-    const upcomingHolidayText = await fetchUpcomingHoliday();
+  if (filepath.startsWith('node_modules/') || filepath.startsWith('.git/')) {
+    return true;
+  }
 
-    if (upcomingHolidayText) {
-      chrome.storage.local.set({
-        'upcomingHolidayText': {
-          text: upcomingHolidayText,
-          timestamp: Date.now(),
-        },
-      });
-      sendResponse({ upcomingHolidayText: upcomingHolidayText });
+  const gitignore = fs.readFileSync(gitignorePath, 'utf8');
+  const ignorePatterns = gitignore.split('\n').filter((line) => line.trim() !== '' && !line.startsWith('#'));
+
+  return ignorePatterns.some((pattern) => new RegExp(`^${pattern.replace('*', '.*')}$`).test(filepath));
+};
+
+const addDirectoryToArchive = (dirPath, archive) => {
+  const items = fs.readdirSync(dirPath);
+  items.forEach((item) => {
+    const itemPath = path.join(dirPath, item);
+    const itemName = path.relative(__dirname, itemPath);
+
+    if (!isIgnored(itemName)) {
+      if (fs.lstatSync(itemPath).isDirectory()) {
+        addDirectoryToArchive(itemPath, archive);
+      } else {
+        archive.file(itemPath, { name: itemName });
+      }
     }
-  }
-  return true; // Required to use sendResponse asynchronously
-});
+  });
+};
+
+const runBrowserify = () => {
+  return new Promise((resolve, reject) => {
+    exec('browserify background.js -o backgroundBundle.js', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error running Browserify: ${error}`);
+        reject(error);
+      } else {
+        console.log('Browserify completed successfully.');
+        resolve();
+      }
+    });
+  });
+};
+
+const createZip = async () => {
+  await runBrowserify();
+
+  const output = fs.createWriteStream(outputPath);
+  const archive = archiver('zip', {
+    zlib: { level: 9 }, // Sets the compression level.
+  });
+
+  output.on('close', () => {
+    console.log(`Extension has been zipped to ${outputPath}. Total size: ${archive.pointer()} bytes.`);
+  });
+
+  archive.on('warning', (err) => {
+    if (err.code === 'ENOENT') {
+      console.warn(err);
+    } else {
+      throw err;
+    }
+  });
+
+  archive.on('error', (err) => {
+    throw err;
+  });
+
+  archive.pipe(output);
+
+  addDirectoryToArchive(__dirname, archive);
+
+  archive.finalize();
+};
+
+createZip();
